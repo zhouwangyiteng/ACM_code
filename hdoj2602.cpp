b45a6e38d940af1976f305cd99ff3b6c94bf243c
zhouwangyiteng <zhouwang122@qq.com>
2016-07-05 15:40:07 +0800

    new file:   hdoj2602.cpp

--- hdoj2602.cpp
+++ hdoj2602.cpp
@@ -8,30 +8,32 @@
 #include<math.h>
 #include<algorithm>
 using namespace std;
-#define MAXN 12888
+#define MAXN 65536
 
-int w[3405],d[3405],dp[MAXN];
+int w[1001],d[1001],dp[1001];
 int MAX(int a,int b)
 {
-	if(a>b)
-		return a;
-	else
-		return b;
+	return a>b?a:b;
 }
 
 int main()
 {
-	int n,m;
-	while(cin>>n>>m)
-	{
+    int t,n,m;
+    cin>>t;
+    while(t--)
+    {
+		cin>>n>>m;
 		for(int i=1;i<=n;i++)
-			cin>>w[i]>>d[i];
+			cin>>w[i];
+		for(int i=1;i<=n;i++)
+			cin>>d[i];
 		memset(dp,0,sizeof(dp));
 		for(int i=1;i<=n;i++)
-			for(int v=m;v>=w[i];v--)
-				dp[v]=MAX(dp[v],dp[v-	w[i]]+d[i]);
+			for(int v=m;v>=d[i];v--)	
+				dp[v]=MAX(dp[v],dp[v-d[i]]+w[i]);
 		cout<<dp[m]<<endl;
+		
 	}
-
+    
 	return 0;  
 }    
