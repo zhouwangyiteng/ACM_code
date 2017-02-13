7bbff508874308ffb7674d61c358258488c98c0a
zhouwangyiteng <zhouwang122@qq.com>
2017-02-13 19:05:06 +0800

    新文件:       BEGIN-3.cpp

---
+++ lanqiao/BEGIN-3.cpp
@@ -0,0 +1,46 @@
+/*************************************************************************
+	> File Name: BEGIN-3.cpp
+	> Author:
+	> Mail:
+	> Created Time: 2017年02月13日 星期一 18时59分15秒
+ ************************************************************************/
+
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <map>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <cmath>
+#include <ctime>
+#include <string>
+#include <cstring>
+#include <set>
+#include <bitset>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <cstdlib>
+#define X first
+#define Y second
+#define LL long long
+#define INF 0x3f3f3f3f
+#define N 150005
+#define M 1000
+#define PB(X) push_back(X)
+#define MP(X,Y) make_pair(X,Y)
+#define CLR(A,X) memset(A,X,sizeof(A))
+using namespace std;
+
+
+int main()
+{
+    double PI=3.14159265358979323;
+    int r;
+    cin>>r;
+    printf("%.7f\n",PI*r*r);
+
+    return 0;
+}
+
