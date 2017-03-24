658d2a62214ccd8d0504f3e4d8b3d928118d6be8
zhouwangyiteng <zhouwang122@qq.com>
2017-03-24 22:24:56 +0800

    新文件:       cf659d.cpp

---
+++ cf659d.cpp
@@ -0,0 +1,47 @@
+/*************************************************************************
+	> File Name: cf659d.cpp
+	> Author:
+	> Mail:
+	> Created Time: 2017年03月24日 星期五 22时20分39秒
+ ************************************************************************/
+
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <map>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <cmath>
+#include <ctime>
+#include <string>
+#include <cstring>
+#include <set>
+#include <bitset>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <cstdlib>
+#define X first
+#define Y second
+#define LL long long
+#define INF 0x3f3f3f3f
+#define N 150005
+#define M 1000
+#define PB(X) push_back(X)
+#define MP(X,Y) make_pair(X,Y)
+#define CLR(A,X) memset(A,X,sizeof(A))
+using namespace std;
+
+int n,a,b;
+
+int main()
+{
+    cin>>n;
+    for(int i=0;i<n;i++)
+        cin>>a>>b;
+    cout<<(n-4)/2<<endl;
+
+    return 0;
+}
+
