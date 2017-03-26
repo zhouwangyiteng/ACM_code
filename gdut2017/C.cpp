9695cf5779e4ea0af9fbc5c3b1eef3b1bf5d2984
zhouwangyiteng <zhouwang122@qq.com>
2017-03-26 21:14:11 +0800

    新文件:       C.cpp

---
+++ gdut2017/C.cpp
@@ -0,0 +1,42 @@
+/*************************************************************************
+	> File Name: C.cpp
+	> Author:
+	> Mail:
+	> Created Time: 2017年03月26日 星期日 13时42分36秒
+ ************************************************************************/
+
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <map>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <cmath>
+#include <ctime>
+#include <string>
+#include <cstring>
+#include <set>
+#include <bitset>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <cstdlib>
+#define X first
+#define Y second
+#define LL long long
+#define INF 0x3f3f3f3f
+#define N 150005
+#define M 1000
+#define PB(X) push_back(X)
+#define MP(X,Y) make_pair(X,Y)
+#define CLR(A,X) memset(A,X,sizeof(A))
+using namespace std;
+
+
+int main()
+{
+
+    return 0;
+}
+
